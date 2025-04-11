e07c76d9484991d2992a417638062994a792042a
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-04-11 13:59:49 +0100

    add domain client uri to allowed origin

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,6 +1,6 @@
 const allowedOrigin = [
   "http://localhost:5173",
-  "https://tumblr-app-clone.vercel.app/",
+  "https://tumblr-app-clone-8xcz.vercel.app",
   "http://127.0.0.1:4000",
 ];
 module.exports = allowedOrigin;
